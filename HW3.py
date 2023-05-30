1e7360b787a0ebd3507285d2a176c46327dadf0b
robert-harrell0708 <109496480+robert-harrell0708@users.noreply.github.com>
2023-05-30 15:04:06 -0400

    HW3 Sign In test case

---
+++ HW3.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.amazon.com/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3F_encoding%3DUTF8%26ref_%3Dnav_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
+
+driver.find_element(By.CSS_SELECTOR, 'h1.a-spacing-small')
+driver.find_element(By.CSS_SELECTOR, 'input#ap_customer_name')
+driver.find_element(By.CSS_SELECTOR, 'input#ap_email')
+driver.find_element(By.CSS_SELECTOR, 'input#ap_password')
+driver.find_element(By.CSS_SELECTOR, 'input#continue')
+driver.find_element(By.CSS_SELECTOR, "a[href='/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088']")
+driver.find_element(By.CSS_SELECTOR, "a[href='/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&nodeId=468496']")
+driver.find_element(By.CSS_SELECTOR, "a[href='/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3F_encoding%3DUTF8%26ref_%3Dnav_newcust&prevRID=PWGAX0YZ7908JPVT8SCA&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0']")
+driver.find_element(By.CSS_SELECTOR, "a[href='https://www.amazon.com/business/register/org/landing?ref_=ap_altreg_ab']")
+
